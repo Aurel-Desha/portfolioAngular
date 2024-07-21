9b0548e4d6f0e95fc9d2d427c58ed761cefd4974
aurel-shael <deshyesguy2@gmail.com>
2024-07-21 11:57:16 +0200

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { OpeningComponent } from './opening/opening.component';
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OpeningComponent
+    OpeningComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
